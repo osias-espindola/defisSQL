c68175022ece67968ce23ffaecde9bbbdc8211a4
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-05-23 10:56:15 +0200

    activite 05_country en cours

--- php/05_countries_count.php
+++ php/05_countries_count.php
@@ -2,9 +2,12 @@
 require_once("./src/connect.php");
 
 // Requête SQL pour récupérer tous les pays, en un seul exemplaire, et le nombre d'utilisateurs par pays
-$sql = "";
-
-
+$sql = "SELECT COUNT(country)
+        FROM users";
+        "SELECT
+            country
+        FROM users ";
+       
 // Préparation de la requête
 $query = $db->prepare($sql);
 // Exécution de la requête
@@ -24,12 +27,12 @@ require_once("./src/close.php");
 </head>
 <body>
     <?php include_once('./components/nav.php') ?>
-    <pre><?= print_r($result) ?></pre>
+<pre><?= print_r($result) ?></pre>
     <div>
         <?php
             foreach ($result as $country) {
                 // faire un echo de chaque pays trouvé avec le nombre d'utilisateurs correspondant
-                
+                echo $country['country'] .'<br>'; 
             }
         ?>
     </div>
