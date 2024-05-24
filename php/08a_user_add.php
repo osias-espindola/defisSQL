9adbc3fb06f12572b09f996d3749b9ff8c440dac
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-05-24 08:38:43 +0200

    activite 08a en cours

--- php/08a_user_add.php
+++ php/08a_user_add.php
@@ -2,7 +2,13 @@
 require_once("./src/connect.php");
 
 // Requête SQL pour insérer un nouvel utilisateur
-$sql = "";
+$sql = 
+"INSERT INTO client (id , first_name , last_name , email, gender , birth_date , country)
+VALUES
+(1001,'Rébecca', 'Armand',Rébecca@hotmail.com, 'Saint-Didier-des-Bois', 2000-04-2024),
+('Aimée', 'Hebert', 'Marigny-le-Châtel', 36),
+('Marielle', 'Ribeiro', 'Maillères', 27),
+('Hilaire', 'Savary', 'Conie-Molitard', 58);";
 
 
 // Préparation de la requête
